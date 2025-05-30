420799d9243e37e86a001e91e255833ac5545d9a
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-05-30 19:06:13 +0200

    feat: check OS in zsh-plugins-apt-get

--- src/zsh-plugins-apt-get/install.sh
+++ src/zsh-plugins-apt-get/install.sh
@@ -2,5 +2,11 @@
 
 set -e
 
+source /etc/os-release
+if [ "$ID" != "debian" ] && [ "$ID" != "ubuntu" ]; then
+	echo "Unsupported distribution "$ID". Only Debian and Ubuntu are supported."
+	exit 1
+fi
+
 apt-get update
 apt-get install -y zsh-autosuggestions zsh-syntax-highlighting
