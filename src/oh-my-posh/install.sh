93ed9e13054d9241d2e4e8ac3d4d8894e65e5fda
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-05-30 19:06:42 +0200

    feat: install oh-my-posh locally for remote user

--- src/oh-my-posh/install.sh
+++ src/oh-my-posh/install.sh
@@ -2,4 +2,6 @@
 
 set -e
 
-curl -s https://ohmyposh.dev/install.sh | bash -s -- -d /usr/bin
+install_path="$_REMOTE_USER_HOME/.local/bin"
+mkdir -p $install_path
+curl -s https://ohmyposh.dev/install.sh | bash -s -- -d $install_path
