238ba34643023cefa29144c4ec133d12851ea60a
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-05-31 16:48:40 +0200

    fix: install oh-my-posh at system level
    Due to a bug, the remote user is not correctly set in the installation
    script when the remote user is created by the common-utils feature.
    
    See https://github.com/devcontainers/cli/issues/331.
    
    Installing at /usr/bin avoids this problem.

--- src/oh-my-posh/install.sh
+++ src/oh-my-posh/install.sh
@@ -2,6 +2,4 @@
 
 set -e
 
-install_path="$_REMOTE_USER_HOME/.local/bin"
-mkdir -p $install_path
-curl -s https://ohmyposh.dev/install.sh | bash -s -- -d $install_path
+curl -s https://ohmyposh.dev/install.sh | bash -s -- -d /usr/bin$install_path
