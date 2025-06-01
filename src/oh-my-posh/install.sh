b348b1f94fd0e18788a9713f4ba22d897e780bec
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-06-01 09:10:40 +0200

    fix: install curl and unzip
    Dropped dependency on external curl feature, as anyway there's no
    feature available for unzip.
    
    Just installing curl and unzip manually, without cleanup for now, to
    keep it simple.

--- src/oh-my-posh/install.sh
+++ src/oh-my-posh/install.sh
@@ -2,4 +2,12 @@
 
 set -e
 
+. /etc/os-release
+if [ "$ID" != "debian" ] && [ "$ID" != "ubuntu" ]; then
+	echo "Unsupported distribution $ID. Only Debian and Ubuntu are supported."
+	exit 1
+fi
+
+apt-get update
+apt-get install -y curl unzip
 curl -s https://ohmyposh.dev/install.sh | bash -s -- -d /usr/bin
