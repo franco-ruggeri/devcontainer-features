ce363084e9154e59ed691f92c6e753a80b337bc0
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-06-01 09:09:37 +0200

    test: format script

--- test/oh-my-posh/test.sh
+++ test/oh-my-posh/test.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if ! command -v oh-my-posh &>/dev/null; then
-  echo "oh-my-posh not found"
-  exit 1
+if ! command -v oh-my-posh >/dev/null 2>&1; then
+	echo "oh-my-posh not found"
+	exit 1
 fi
