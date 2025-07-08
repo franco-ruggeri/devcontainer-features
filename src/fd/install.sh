acd55b8ada8573ad1d9eabc5bf0b418ddc305249
Franco Ruggeri <franco.ruggeri.pro@gmail.com>
2025-07-08 18:41:48 +0200

    fix: add symlink fd -> fdfind

--- src/fd/install.sh
+++ src/fd/install.sh
@@ -10,3 +10,8 @@ fi
 
 apt-get update
 apt-get install -y fd-find
+
+# On Ubuntu, fd is installed as `fdfind`
+# Create a symlink to make it available as `fd`
+# See https://github.com/sharkdp/fd?tab=readme-ov-file#on-ubuntu
+ln -s $(which fdfind) /usr/bin/fd
